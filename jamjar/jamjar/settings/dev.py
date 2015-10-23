db59c4cca1962cf3560a02b6f7876f7d8fbaf9da
Mark Koh <mkoh@rjmetrics.com>
2015-10-23 03:14:33 +0000

    Set up database stuffs and fixed setting jawns.

---
+++ jamjar/jamjar/settings/dev.py
@@ -0,0 +1,12 @@
+from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jamjar',
+        'USER': 'jamjar',
+        'PASSWORD': 'jamjar',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
