b20c82433fbe76bbd519ee2704bfe519840e1664
Drew Banin <dsb59@drexel.edu>
2015-10-23 03:15:45 +0000

    move around mysql create statements

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ source env/bin/activate
 pip install -r requirements.txt
 echo "Requirements installed"
 
-create_user="GRANT ALL ON *.* TO 'jmajar'@'localhost' IDENTIFIED BY 'jamjar';"
-mysql -uroot -proot -e "$create_user"
-echo "User maadbox with password 'password' created"
-
 create_db="CREATE DATABASE IF NOT EXISTS jamjar"
 mysql -uroot -proot -e "$create_db"
 echo "created jamjar database"
+
+create_user="GRANT ALL ON *.* TO 'jamjar'@'localhost' IDENTIFIED BY 'jamjar';"
+mysql -uroot -proot -e "$create_user"
+echo "User maadbox with password 'password' created"
+
