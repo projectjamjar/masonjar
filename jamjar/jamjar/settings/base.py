2f3056a59b799a7f41a1736775f09c953776dccd
Mark Koh <mkoh@rjmetrics.com>
2016-01-18 23:49:37 +0000

    Fied sessiontoken jawn

--- jamjar/jamjar/settings/base.py
+++ jamjar/jamjar/settings/base.py
@@ -79,7 +79,6 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
 }
