0371248ee429e0beaa258076aeeed6946f1f5e84
Mark Koh <mkoh@rjmetrics.com>
2016-01-19 00:27:22 +0000

    Apt-install jawns.

--- apt-install.sh
+++ apt-install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo "Please run this script as sudo."
+    exit 1
+fi
+
 apt-get update
 apt-get install -y apache2 python-dev python-pip python-virtualenv libmysqlclient-dev zip 
+apt-get install -y python-numpy python-scipy python-matplotlib libportaudio-dev python-PyAudio python-MySQLdb libav-tools
 
 a2enmod proxy_http
 cp apache2.conf /etc/apache2/
