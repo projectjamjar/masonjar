a4933a71b6f905f5c8eb0116eca0d83197ec1008
Mark Koh <mkoh@rjmetrics.com>
2016-01-19 01:06:43 +0000

    Apt installs still wrong.

--- apt-install.sh
+++ apt-install.sh
@@ -6,8 +6,9 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 apt-get update
-apt-get install -y apache2 python-dev python-pip python-virtualenv libmysqlclient-dev zip 
+apt-get install -y apache2 python-dev python-pip python-virtualenv libmysqlclient-dev zip
 apt-get install -y python-numpy python-scipy python-matplotlib libportaudio-dev python-PyAudio python-MySQLdb libav-tools
+apt-get install -y pkg-config libblas-dev liblapack-dev libatlas-base-dev gfortran
 
 a2enmod proxy_http
 cp apache2.conf /etc/apache2/
