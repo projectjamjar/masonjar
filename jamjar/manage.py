9639c1070e43b425b5617b0b5e845a8c658a4ba1
Mark Koh <markkohdev@gmail.com>
2016-01-21 19:29:37 +0000

    Added some more dependencies.

--- jamjar/manage.py
+++ jamjar/manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if 'test' in sys.argv:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jamjar.settings.test")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jamjar.settings.dev")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jamjar.settings.prod")
 
     from django.core.management import execute_from_command_line
 
