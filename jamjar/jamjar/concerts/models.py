369768fb696f7c16c54dc7a5c847d1274bf3cc43
Drew Banin <dsb59@drexel.edu>
2016-02-01 03:27:45 +0000

    move graph gen to concert model

--- jamjar/jamjar/concerts/models.py
+++ jamjar/jamjar/concerts/models.py
@@ -1,6 +1,64 @@
 from django.db import models
+from django.forms.models import model_to_dict
+
 from jamjar.base.models import BaseModel
+from jamjar.videos.models import Edge
+
+from collections import defaultdict
 
 class Concert(BaseModel):
     date = models.DateField()
     venue = models.ForeignKey('venues.Venue',related_name='concerts')
+
+    def make_graph(self):
+        concert_id = self.id
+        concert_edges = Edge.objects.filter(video1__concert_id=concert_id).select_related('video1', 'video2')
+
+        # create a mapping of video_id --> video for later
+        video_map = {}
+        edge_map  = {}
+        for edge in concert_edges:
+            video_map[edge.video1.id] = edge.video1
+            video_map[edge.video2.id] = edge.video2
+
+            edge_map[(edge.video1.id, edge.video2.id)] = edge
+
+        # build an adjacency list to help with generating the graph
+        video_adjacencies = defaultdict(list)
+        for edge in concert_edges:
+            video_adjacencies[edge.video1.id].append(edge.video2)
+            video_adjacencies[edge.video2.id].append(edge.video1)
+
+        graph = []
+        for (video_id, adjacents) in video_adjacencies.iteritems():
+            video = video_map[video_id]
+
+            connects_to = []
+            for adj_video in adjacents:
+                index = (adj_video.id, video_id)
+                if index in edge_map:
+                    offset = edge_map[index].offset
+                    confidence = edge_map[index].confidence
+                elif index[::-1] in edge_map:
+                    index = index[::-1]
+                    offset = -edge_map[index].offset
+                    confidence = edge_map[index].confidence
+                else:
+                    raise RuntimeError("this shouldn't happen")
+
+                data = {
+                    "video" : model_to_dict(adj_video),
+                    "edge"  : {
+                        "offset" : offset,
+                        "confidence" : confidence
+                    }
+                }
+                connects_to.append(data)
+
+            data = {
+                "video": model_to_dict(video),
+                "conncects_to" : connects_to
+            }
+            graph.append(data)
+
+        return graph
--- jamjar/jamjar/videos/tests/test_edge.py
+++ jamjar/jamjar/videos/tests/test_edge.py
@@ -12,18 +12,24 @@ TEST_VIDEO_PATH = os.path.join(os.path.dirname(__file__), 'mark-dancing.mp4')
 
 class EdgeTestCase(TestCase):
     def setUp(self):
+        self.venue = Venue(name="MET Lab", lat=10.0, lng=20.0, website="http://projectjamjar.com", utc_offset=12)
+        self.venue.save()
 
-        venue = Venue(name="MET Lab")
-        venue.save()
+        self.concert = Concert(date="2016-01-01", venue=self.venue)
+        self.concert.save()
 
-        concert = Concert(date="2016-01-01", venue=venue)
-        concert.save()
-
-        self.video1 = Video(concert_id=concert.id, length=2)
+        self.video1 = Video(concert_id=self.concert.id, length=10.0)
         self.video1.save()
 
-        self.video2 = Video(concert_id=concert.id, length=2)
+        self.video2 = Video(concert_id=self.concert.id, length=10.0)
         self.video2.save()
+
+        self.video3 = Video(concert_id=self.concert.id, length=10.0)
+        self.video3.save()
+
+        self.edge1 = Edge.new(video1_id=self.video1.id, video2_id=self.video2.id, confidence=10, offset=20.0)
+        self.edge2 = Edge.new(video1_id=self.video2.id, video2_id=self.video3.id, confidence=11, offset=10.0)
+        self.edge3 = Edge.new(video1_id=self.video1.id, video2_id=self.video3.id, confidence=12, offset=30.0)
 
     def test_edge(self):
         edge = Edge.new(self.video1.id, self.video2.id, 0, 1)
