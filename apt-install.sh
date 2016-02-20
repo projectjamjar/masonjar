dbb3629e94e9f2b0149b852502c17610370444f5
Mark Koh <mkoh@drexel.edu>
2016-02-20 17:20:33 +0000

    Updated readme and apt-install

--- apt-install.sh
+++ apt-install.sh
@@ -11,6 +11,11 @@ apt-get install -y python-numpy python-scipy python-matplotlib libportaudio-dev 
 apt-get install -y pkg-config libblas-dev liblapack-dev libatlas-base-dev gfortran libpng-dev libfreetype6-dev imagemagick
 apt-get install -y portaudio19-dev
 
+# Install autoenv
+pip install virtualenv autoenv
+virtualenv env
+echo "source `which activate.sh`" >> ~/.bashrc
+
 a2enmod proxy_http
 cp apache2.conf /etc/apache2/
 service apache2 restart
@@ -20,4 +25,3 @@ wget -O /tmp/redis-3.0.6.tar.gz http://download.redis.io/releases/redis-3.0.6.ta
 tar -xvf /tmp/redis-3.0.6.tar.gz -C /tmp/
 make -C /tmp/redis-3.0.6/
 make -C /tmp/redis-3.0.6/ install
-
