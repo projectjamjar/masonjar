2e2b8ef1a0cd8047a63131f83423590ab0dd6dc9
Mark Koh <markkohdev@gmail.com>
2016-04-11 02:36:22 +0000

    Added artist popularity and followers to model.  Added update_spotify_info method.

--- jamjar/jamjar/artists/models.py
+++ jamjar/jamjar/artists/models.py
@@ -10,7 +10,49 @@ class Artist(BaseModel):
     name = models.CharField(max_length=150)
     spotify_id = models.CharField(max_length=100, null=True)
     genres = models.ManyToManyField('artists.Genre',related_name='artists',blank=True)
+    popularity = models.IntegerField(null=True) # Popularity (0-100) on spotify
+    followers = models.IntegerField(null=True) # Number of followers on spotify
     unofficial = models.BooleanField(default=False)
+
+
+    def save_spotify_info(self, spotify_artist):
+        if spotify_artist != None:
+            try:
+                # Create an artist object
+                self.name = spotify_artist.get('name')
+                self.popularity = spotify_artist.get('popularity')
+                self.followers = spotify_artist.get('followers',{}).get('total')
+                self.genres = [] # Clear out the genres
+                self.save()
+
+                genres = spotify_artist.get('genres',[])
+                for genre in genres:
+                    # Get or create the genre and add it
+                    (genre, created) = Genre.objects.get_or_create(name=genre)
+                    self.genres.add(genre)
+
+                # Delete all old images
+                self.images.all().delete()
+
+                images = spotify_artist.get('images',[])
+                for image in images:
+                    # Add the image
+                    image_obj = ArtistImage.objects.create(artist=self,**image)
+                    self.images.add(image_obj)
+            except IntegrityError, e:
+                logger.warn('Integrity error caught during artist update: {}'.format(e))
+
+    def update_spotify_info(self):
+        if self.spotify_id != None:
+            try:
+                sp = spotipy.Spotify()
+                spotify_artist = sp.artist(self.spotify_id)
+
+                # Save that ish to the jawn
+                self.save_spotify_info(spotify_artist)
+
+            except spotipy.SpotifyException, e:
+                logger.warn('Spotify error caught during artist update: {}'.format(e))
 
     @staticmethod
     def search_artist(search_string):
@@ -30,6 +72,7 @@ class Artist(BaseModel):
             artist = cls.objects.get(spotify_id=spotify_id)
         except cls.DoesNotExist, e:
             # We don't have this artist in our DB yet.  Get em!
+            # NOTE: We need to be careful of race conditions here!!!
             try:
                 sp = spotipy.Spotify()
                 spotify_artist = sp.artist(spotify_id)
@@ -37,22 +80,10 @@ class Artist(BaseModel):
                 if spotify_artist != None:
                     # Create an artist object
                     artist = cls()
-                    artist.name = spotify_artist.get('name')
-                    artist.spotify_id = spotify_id
                     artist.save()
 
-                    genres = spotify_artist.get('genres',[])
-                    for genre in genres:
-                        # Get or create the genre and add it
-                        (genre, created) = Genre.objects.get_or_create(name=genre)
-                        artist.genres.add(genre)
-
-                    images = spotify_artist.get('images',[])
-                    for image in images:
-                        # Add the image
-                        image_obj = ArtistImage.objects.create(artist=artist,**image)
-                        artist.images.add(image_obj)
-
+                    # Save that sweet, sweet, spotify info
+                    artist.save_spotify_info(spotify_artist)
                 else:
                     return None
             except IntegrityError, e:
