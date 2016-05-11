6a7ab71b78f8a5e7b7979f662a6fdfe091181be0
Drew Banin <dsb59@drexel.edu>
2016-05-11 23:50:29 +0000

    change default mysql password

--- database_setup.sh
+++ database_setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 create_db="CREATE DATABASE IF NOT EXISTS jamjar"
-mysql -uroot -proot -e "$create_db"
+mysql -uroot -prootpass -e "$create_db"
 echo "created jamjar database"
 
 create_user="GRANT ALL ON *.* TO 'jamjar'@'localhost' IDENTIFIED BY 'jamjar';"
-mysql -uroot -proot -e "$create_user"
+mysql -uroot -prootpass -e "$create_user"
 echo "User maadbox with password 'password' created"
 
 create_db="CREATE DATABASE IF NOT EXISTS dejavu"
-mysql -uroot -proot -e "$create_db"
+mysql -uroot -prootpass -e "$create_db"
 echo "created dejavu database"
 
 create_db="CREATE DATABASE IF NOT EXISTS dejavu_test"
-mysql -uroot -proot -e "$create_db"
+mysql -uroot -prootpass -e "$create_db"
 echo "created dejavu_test database"
 
 create_user="GRANT ALL ON *.* TO 'jamjar'@'localhost' IDENTIFIED BY 'jamjar';"
-mysql -uroot -proot -e "$create_user"
+mysql -uroot -prootpass -e "$create_user"
 echo "User jamjar with password 'jamjar' created"
 
 
