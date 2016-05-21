df55a9d6147b166c792a4b8cacf32ed51e221d01
Drew Banin <dsb59@drexel.edu>
2016-05-21 07:20:53 +0000

    sort jampicks by id

--- jamjar/jamjar/videos/views.py
+++ jamjar/jamjar/videos/views.py
@@ -397,6 +397,7 @@ class JamPickView(BaseView):
         # Get all da videos that have a non-null jampick
         queryset = Video.objects.for_user(request.user).filter(jampick__isnull=False)
         queryset = queryset.select_related('user', 'concert', 'concert__venue').prefetch_related('artists', 'artists__genres', 'artists__images', 'concert__artists')
+        queryset = queryset.order_by('jampick__id')
 
         expanded_serializer = ExpandedVideoSerializer(queryset, many=True, context={'request': request})
 
